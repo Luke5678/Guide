f290afe2ffe90bc8a9874b06cfd99053f4cb481e
Luke5678 <lukaszg257@gmail.com>
2024-05-18 18:36:06 +0200

    ...

---
+++ src/Guide.Shared/Common/Interfaces/IAttractionService.cs
@@ -0,0 +1,10 @@
+﻿using Guide.Shared.Common.Dtos;
+
+namespace Guide.Shared.Common.Interfaces;
+
+public interface IAttractionService
+{
+    Task<AttractionDto?> GetById(int id);
+    Task<List<AttractionDto>> Get(int page = 0, int limit = 0, string? orderBy = null, string? search = null);
+    Task<int> GetCount(string? search = null);
+}
